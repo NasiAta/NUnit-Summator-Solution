9fa00d7054d7b475a7d5965e3945cb59dfb18187
NasiAta <105272391+NasiAta@users.noreply.github.com>
2022-05-11 15:02:16 +0300

    Add the folder with the solution

---
+++ Summator/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Summator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Summator.Sum(new long[] { 1, 2, 3, 4, 5, 6 });
+            if (result == 21)
+            {
+                Console.WriteLine("Test PASS");
+            }
+            else
+            {
+                Console.WriteLine("Test FAIL");
+            }
+            {
+                int average = Summator.average(new int[] { 5, 10, 15 });
+                if (average == 10)
+                {
+                    Console.WriteLine("Test PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test FAIL");
+                }
+            }
+        }
+    }
+}
+
+
+
